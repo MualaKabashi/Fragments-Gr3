a992a8d23fbf456ccb06eb6a378d7448971edbd8
Muala Kabashi <mualaakabashi@gmail.com>
2024-12-09 17:18:32 +0100

    first commit

---
+++ app/src/main/java/com/cacttus/fragments_gr3/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cacttus.fragments_gr3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
+
