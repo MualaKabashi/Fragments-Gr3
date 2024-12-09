f403a27645e1734c3db725c45450f3b99e3f60df
Muala Kabashi <keds30955@KEDS.kv>
2024-12-09 18:40:45 +0100

    fragments

---
+++ app/src/main/java/com/cacttus/fragments_gr3/fragments/FirstFragment.kt
@@ -0,0 +1,18 @@
+package com.cacttus.fragments_gr3.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.cacttus.fragments_gr3.R
+
+class FirstFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.first_fragment, container, false)
+    }
+}
